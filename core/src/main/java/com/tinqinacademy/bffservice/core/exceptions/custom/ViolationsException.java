ee511759a62b984be0bef575c7a9974d5b9c0e0f
yordanov0502 <yordanovtodor281@gmail.com>
2024-08-05 09:06:16 +0300

    core module updated

---
+++ core/src/main/java/com/tinqinacademy/bffservice/core/exceptions/custom/ViolationsException.java
@@ -0,0 +1,19 @@
+package com.tinqinacademy.bffservice.core.exceptions.custom;
+
+import com.tinqinacademy.bffservice.api.error.Error;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter
+public class ViolationsException extends RuntimeException{
+
+    private final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+    private final List<Error> errorList;
+
+    public ViolationsException(String message, List<Error> errorList) {
+        super(message);
+        this.errorList = errorList;
+    }
+}
---
+++ core/src/main/java/com/tinqinacademy/bffservice/core/operations/BaseOperationProcessor.java
@@ -0,0 +1,39 @@
+package com.tinqinacademy.bffservice.core.operations;
+
+import com.tinqinacademy.bffservice.api.base.OperationInput;
+import com.tinqinacademy.bffservice.api.error.Error;
+import com.tinqinacademy.bffservice.core.exceptions.ExceptionService;
+import com.tinqinacademy.bffservice.core.exceptions.custom.ViolationsException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public abstract class BaseOperationProcessor {
+
+    protected final ConversionService conversionService;
+    protected final ExceptionService exceptionService;
+    private final Validator validator;
+
+    protected <I extends OperationInput> void validate(I input){
+        Set<ConstraintViolation<I>> violationSet = validator.validate(input);
+
+        if(!violationSet.isEmpty()){
+        List<Error> errorList = violationSet
+                .stream()
+                .map(violation -> Error.builder()
+                        .field(violation.getPropertyPath().toString())
+                        .errMsg(violation.getMessage())
+                        .build())
+                .collect(Collectors.toList());
+        throw new ViolationsException("Violation exception occurred.",errorList);
+        }
+    }
+}
