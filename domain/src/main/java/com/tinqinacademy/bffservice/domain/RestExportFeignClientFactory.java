44cba4e726a368d5cc202df60af434cec3dbe220
Todor Yordanov <yordanovtodor281@gmail.com>
2024-08-07 10:51:54 +0300

    [T4] application.properties file updated to use .env file to hide sensitive data (#8)

--- domain/src/main/java/com/tinqinacademy/bffservice/domain/RestExportFeignClientFactory.java
+++ domain/src/main/java/com/tinqinacademy/bffservice/domain/RestExportFeignClientFactory.java
@@ -3,12 +3,12 @@ package com.tinqinacademy.bffservice.domain;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tinqinacademy.commentsservice.restexport.CommentsRestExport;
 import com.tinqinacademy.hotel.restexport.HotelRestExport;
-import feign.Contract;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import feign.okhttp.OkHttpClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,26 +17,28 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class RestExportFeignClientFactory {
 
+    @Value("${env.HOTEL_SERVICE_URL}")
+    private String HOTEL_SERVICE_URL;
+    @Value("${env.COMMENTS_SERVICE_URL}")
+    private String COMMENTS_SERVICE_URL;
     private final ApplicationContext applicationContext;
-
-    @Bean
-    public CommentsRestExport buildCommentsFeignClient() {
-        return Feign.builder()
-                .contract(new Contract.Default())
-                .client(new OkHttpClient())
-                .encoder(new JacksonEncoder(applicationContext.getBean(ObjectMapper.class)))
-                .decoder(new JacksonDecoder(applicationContext.getBean(ObjectMapper.class)))
-                .target(CommentsRestExport.class,"http://localhost:8081");
-    }
 
     @Bean
     public HotelRestExport buildHotelFeignClient() {
         return Feign.builder()
-                .contract(new Contract.Default())
                 .client(new OkHttpClient())
                 .encoder(new JacksonEncoder(applicationContext.getBean(ObjectMapper.class)))
                 .decoder(new JacksonDecoder(applicationContext.getBean(ObjectMapper.class)))
-                .target(HotelRestExport.class,"http://localhost:8080");
+                .target(HotelRestExport.class,HOTEL_SERVICE_URL);
+    }
+
+    @Bean
+    public CommentsRestExport buildCommentsFeignClient() {
+        return Feign.builder()
+                .client(new OkHttpClient())
+                .encoder(new JacksonEncoder(applicationContext.getBean(ObjectMapper.class)))
+                .decoder(new JacksonDecoder(applicationContext.getBean(ObjectMapper.class)))
+                .target(CommentsRestExport.class,COMMENTS_SERVICE_URL);
     }
 
 }
--- rest/src/main/java/com/tinqinacademy/bffservice/rest/BffServiceApplication.java
+++ rest/src/main/java/com/tinqinacademy/bffservice/rest/BffServiceApplication.java
@@ -3,16 +3,12 @@ package com.tinqinacademy.bffservice.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = "com.tinqinacademy.bffservice")
-//@EnableFeignClients(basePackages = "com.tinqinacademy.bffservice.domain")
 public class BffServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BffServiceApplication.class, args);
     }
-
 }
