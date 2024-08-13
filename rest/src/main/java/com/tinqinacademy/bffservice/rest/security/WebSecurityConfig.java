1be06e46c8445adc15975991ce9427923e1bf002
yordanov0502 <yordanovtodor281@gmail.com>
2024-08-13 13:41:18 +0300

    Security filter chain updated

--- rest/src/main/java/com/tinqinacademy/bffservice/rest/security/WebSecurityConfig.java
+++ rest/src/main/java/com/tinqinacademy/bffservice/rest/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.tinqinacademy.bffservice.api.exceptions.custom.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,23 +22,6 @@ public class WebSecurityConfig {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAuthFilter jwtAuthFilter;
 
-    private final String[] ADMIN_URLS = {
-            RestApiRoutes.REGISTER_VISITOR,
-            RestApiRoutes.GET_VISITORS,
-            RestApiRoutes.CREATE_ROOM,
-            RestApiRoutes.UPDATE_ROOM,
-            RestApiRoutes.UPDATE_ROOM_PARTIALLY,
-            RestApiRoutes.DELETE_ROOM,
-            RestApiRoutes.ADMIN_EDIT_COMMENT_FOR_ROOM,
-            RestApiRoutes.ADMIN_DELETE_COMMENT_FOR_ROOM
-    };
-    private final String[] USER_URLS = {
-            RestApiRoutes.BOOK_ROOM,
-            RestApiRoutes.UNBOOK_ROOM,
-            RestApiRoutes.ADD_COMMENT_FOR_ROOM,
-            RestApiRoutes.EDIT_COMMENT_FOR_ROOM
-    };
-
     @Bean
     UserDetailsService emptyDetailsService() {
         return username -> { throw new NotFoundException("No local users, only JWT tokens allowed."); };
@@ -47,12 +31,34 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(ADMIN_URLS).hasRole("ADMIN"));
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(USER_URLS).hasAnyRole("USER", "ADMIN"));
+        secureAdminEndpoints(http);
+        secureUserEndpoints(http);
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
         http.exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint));
         http.sessionManagement((sessionManagement)-> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    private void secureAdminEndpoints(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, RestApiRoutes.REGISTER_VISITOR).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, RestApiRoutes.GET_VISITORS).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, RestApiRoutes.CREATE_ROOM).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, RestApiRoutes.UPDATE_ROOM).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, RestApiRoutes.UPDATE_ROOM_PARTIALLY).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, RestApiRoutes.DELETE_ROOM).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, RestApiRoutes.ADMIN_EDIT_COMMENT_FOR_ROOM).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, RestApiRoutes.ADMIN_DELETE_COMMENT_FOR_ROOM).hasRole("ADMIN")
+        );
+    }
+
+    private void secureUserEndpoints(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, RestApiRoutes.BOOK_ROOM).hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE, RestApiRoutes.UNBOOK_ROOM).hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, RestApiRoutes.ADD_COMMENT_FOR_ROOM).hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.PATCH, RestApiRoutes.EDIT_COMMENT_FOR_ROOM).hasAnyRole("USER", "ADMIN")
+        );
     }
 }
