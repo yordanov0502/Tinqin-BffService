3470764e1b4f66652efaa739f95b8d366afd6d0f
yordanov0502 <yordanovtodor281@gmail.com>
2024-08-17 09:16:38 +0300

    [T17] Feature for room deletion added

---
+++ api/src/main/java/com/tinqinacademy/bffservice/api/operations/hotelservice/system/deleteroom/DeleteRoomBffOutput.java
@@ -0,0 +1,13 @@
+package com.tinqinacademy.bffservice.api.operations.hotelservice.system.deleteroom;
+
+import com.tinqinacademy.bffservice.api.base.OperationOutput;
+import lombok.*;
+
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class DeleteRoomBffOutput implements OperationOutput {
+
+}
---
+++ api/src/main/java/com/tinqinacademy/bffservice/api/operations/hotelservice/system/deleteroom/DeleteRoomOperation.java
@@ -0,0 +1,6 @@
+package com.tinqinacademy.bffservice.api.operations.hotelservice.system.deleteroom;
+
+import com.tinqinacademy.bffservice.api.base.OperationProcessor;
+
+public interface DeleteRoomOperation extends OperationProcessor<DeleteRoomBffInput, DeleteRoomBffOutput> {
+}
