d3bd317d3417fc0f2948ff2e7b29f9b5cb732bb5
Todor Yordanov <yordanovtodor281@gmail.com>
2024-08-18 20:25:13 +0300

    [T18] Feature for adding a comment to a room added (#35)

---
+++ api/src/main/java/com/tinqinacademy/bffservice/api/operations/commentsservice/system/addcommentforroom/AddCommentForRoomOperation.java
@@ -0,0 +1,6 @@
+package com.tinqinacademy.bffservice.api.operations.commentsservice.system.addcommentforroom;
+
+import com.tinqinacademy.bffservice.api.base.OperationProcessor;
+
+public interface AddCommentForRoomOperation extends OperationProcessor<AddCommentForRoomBffInput, AddCommentForRoomBffOutput> {
+}
---
+++ core/src/main/java/com/tinqinacademy/bffservice/core/converters/comments/AddCommentForRoomBffInputToCommentInput.java
@@ -0,0 +1,24 @@
+package com.tinqinacademy.bffservice.core.converters.comments;
+
+import com.tinqinacademy.bffservice.api.operations.commentsservice.system.addcommentforroom.AddCommentForRoomBffInput;
+import com.tinqinacademy.bffservice.core.converters.BaseConverter;
+import com.tinqinacademy.commentsservice.api.operations.hotel.addcommentforroom.AddCommentForRoomInput;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddCommentForRoomBffInputToCommentInput extends BaseConverter<AddCommentForRoomBffInput, AddCommentForRoomInput,AddCommentForRoomBffInputToCommentInput> {
+    public AddCommentForRoomBffInputToCommentInput() {
+        super(AddCommentForRoomBffInputToCommentInput.class);
+    }
+
+    @Override
+    protected AddCommentForRoomInput convertObj(AddCommentForRoomBffInput bffInput) {
+        AddCommentForRoomInput commentInput = AddCommentForRoomInput.builder()
+                .roomId(bffInput.getRoomId())
+                .userId(bffInput.getUserId())
+                .content(bffInput.getContent())
+                .build();
+
+        return commentInput;
+    }
+}
