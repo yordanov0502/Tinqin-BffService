336a3709b0593e1cf9962992e08c6dfbce849630
Todor Yordanov <yordanovtodor281@gmail.com>
2024-08-27 11:20:12 +0300

    [T27] Aspect for logging updated (#53)

--- core/src/main/java/com/tinqinacademy/bffservice/core/aspects/LoggingAspect.java
+++ core/src/main/java/com/tinqinacademy/bffservice/core/aspects/LoggingAspect.java
@@ -1,9 +1,8 @@
 package com.tinqinacademy.bffservice.core.aspects;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -17,22 +16,40 @@ public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* com.tinqinacademy.bffservice.core.operations..*.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        String requestId = MDC.get("requestId");
+    @Around("execution(* com.tinqinacademy.bffservice.core.operations..*.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        Object[] args = joinPoint.getArgs();
+        String requestId = MDC.get("requestId");
         String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
         String inputArgs = Arrays.toString(args);
 
         logger.info("==> Start requestId: {}. Method: {}(). Input: {}", requestId, methodName, inputArgs);
-    }
 
-    @AfterReturning(pointcut = "execution(* com.tinqinacademy.bffservice.core.operations..*.*(..))", returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        String requestId = MDC.get("requestId");
-        String methodName = joinPoint.getSignature().getName();
+        Object result;
+        result = joinPoint.proceed();
 
         logger.info("<== End requestId: {}. Method: {}(). Output: {}", requestId, methodName, result);
+
+        return result;
     }
+
+//    @Before("execution(* com.tinqinacademy.bffservice.core.operations..*.*(..))")
+//    public void logBefore(JoinPoint joinPoint) {
+//        String requestId = MDC.get("requestId");
+//
+//        Object[] args = joinPoint.getArgs();
+//        String methodName = joinPoint.getSignature().getName();
+//        String inputArgs = Arrays.toString(args);
+//
+//        logger.info("==> Start requestId: {}. Method: {}(). Input: {}", requestId, methodName, inputArgs);
+//    }
+//
+//    @AfterReturning(pointcut = "execution(* com.tinqinacademy.bffservice.core.operations..*.*(..))", returning = "result")
+//    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+//        String requestId = MDC.get("requestId");
+//        String methodName = joinPoint.getSignature().getName();
+//
+//        logger.info("<== End requestId: {}. Method: {}(). Output: {}", requestId, methodName, result);
+//    }
 }
